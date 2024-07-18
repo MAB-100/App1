fe719546012ca18db7c5a86ce3aa96b949ab3e43
MAB-100 <shahabbas661@gmail.com>
2024-07-18 18:42:31 +0500

    Webapp interface updated

--- webapp.py
+++ webapp.py
@@ -1,16 +1,25 @@
 import streamlit as st
 import functions
 
-
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    print(todo)
+    todos.append(todo)
+    functions.write_todos(todos)
 
 st.title("TODOs App")
 st.subheader("Welcome to the TODOs App")
 st.write("This is a simple TODOs App built using Python and Streamlit")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-
-st.text_input("", placeholder="Enter a todo....")
+st.text_input(label="", placeholder="Enter a todo....", 
+              on_change=add_todo,key="new_todo")
